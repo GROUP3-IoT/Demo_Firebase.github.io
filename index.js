668106046291acf8869c6968afbdb951a44c8f18
GROUP3-IoT <116048221+GROUP3-IoT@users.noreply.github.com>
2022-11-01 09:26:58 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDT0mYGAsbltaruVibpSg50EiZPuetc5xU",
+  authDomain: "group3-iot.firebaseapp.com",
+  databaseURL: "https://group3-iot-default-rtdb.firebaseio.com",
+  projectId: "group3-iot",
+  storageBucket: "group3-iot.appspot.com",
+  messagingSenderId: "1047825340639",
+  appId: "1:1047825340639:web:75db42bfe8a77b8a32b16a"
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+send.addEventListener('click',(e) =>{ 
+Sends();
+});
+read.addEventListener('click',(e) =>{ 
+Getval();
+});
+
+function Sends(){
+    Number = parseFloat(document.getElementById('Number').value);
+const db = getDatabase();
+  set(ref(db, 'Number/'), {
+    Value : Number
+  });
+}
+
+function Getval(){
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `Number` + '/Value')).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        document.getElementById('val').innerHTML = "Number/Value="+snapshot.val();
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+}
